4eb3fa678c33f68aedc6db61e53af1fc4286202a
dani <daniele.davino@live.it>
2023-09-24 03:45:04 +0200

    add instructions

--- prod/app.py
+++ prod/app.py
@@ -1,60 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# from google.cloud import bigquery
-# import pandas_gbq
-# from datetime import datetime, timedelta
-
-# st.set_page_config(layout="wide")
-
-
-# days_mapping = {
-#     'Monday': 'lunedì',
-#     'Tuesday': 'martedì',
-#     'Wednesday': 'mercoledì',
-#     'Thursday': 'giovedì',
-#     'Friday': 'venerdì',
-#     'Saturday': 'sabato',
-#     'Sunday': 'domenica'
-# }
-
-# project_id = "bigquery-to-streamlit"
-# dataset_id = "turni_babbuz"
-# table_id = "df"
-
-# st.title("Turni Babbuz")
-
-# # SQL query to select data from the table and order by date
-# sql = """
-# SELECT * 
-# FROM `{}.{}`
-# ORDER BY data
-# """.format(dataset_id, table_id)
-
-# # Load data into a DataFrame
-# df = pandas_gbq.read_gbq(sql, project_id=project_id)
-
-# # Check if the dataframe's first row is "yesterday's date"
-# yesterday = datetime.now().date() - timedelta(days=1)
-# st.write(yesterday)
-# st.write(df.dtypes)
-# if df['data'].iloc[0] == yesterday:
-#     # Remove the first row (yesterday's date)
-#     df = df.iloc[1:].reset_index(drop=True)
-
-# df = df.set_index("data")
-
-# def color_rows(row):
-#     color = '#e6ffe6' if row['giorno'] in ['sabato', 'domenica'] else ''  
-#     return ['background-color: {}'.format(color) for _ in row]
-
-# df = df.style.apply(color_rows, axis=1)
-
-# df = st.data_editor(df,use_container_width=True, disabled=("data","giorno"))
-
-# if st.button('Save to BigQuery'):
-#     df.to_gbq('{}.{}'.format(dataset_id, table_id), project_id, if_exists='replace')
-#     st.success("Data saved to BigQuery successfully!")
-
 from hmac import new
 import streamlit as st
 import pandas as pd
@@ -79,6 +22,8 @@ dataset_id = "turni_babbuz"
 table_id = "df"
 
 st.title("Turni Babbuz")
+
+st.write("Cliccate due volte sulla cella in cui volete scrivere.")
 
 # SQL query to select data from the table and order by date
 sql = f"""
